2b9d04a525f9f0b5cc4a820b832a3f6dcf357736
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-03-20 14:49:33 -0700

    Create BinaryTreeVerticalOrder.java

---
+++ BinaryTreeVerticalOrder.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+       int height = getHeight(root);
+        //total buckets
+       int totalBuckets =  2 * height - 1;
+       int rootIndex =  height - 1;
+       Queue<Node>queue = new LinkedList<>();
+       queue.add(new Node(root, rootIndex));
+       List<List<Integer>>result = new ArrayList<>();
+        
+        //initialized all the bucekts;
+        for(int i = 0; i < totalBuckets; i++){
+            result.add(new ArrayList<>());
+        }
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                Node curr = queue.poll();
+                int index = curr.yCoordinator;
+                result.get(index).add(curr.root.val);
+                if(curr.root.left != null){
+                    queue.add(new Node(curr.root.left,index - 1));
+                }
+                if(curr.root.right != null){
+                    queue.add(new Node(curr.root.right,index + 1));
+                }
+            }
+        }
+        result.removeIf(ele -> (ele.size() == 0));
+        return result;
+    }
+    private int getHeight(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return Math.max(getHeight(root.left) , getHeight(root.right)) + 1;
+    }
+    private class Node{
+        TreeNode root;
+        int yCoordinator;
+        public Node(TreeNode root, int yCoordinator){
+            this.root = root;
+            this.yCoordinator = yCoordinator;
+        }
+    }
+    
+}
