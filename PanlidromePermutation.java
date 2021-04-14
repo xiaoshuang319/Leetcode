f1325ec79a4b25c7aebdbbabd30cd3e3aed5ed06
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-04-14 08:53:26 -0700

    Update PanlidromePermutation.java

--- PanlidromePermutation.java
+++ PanlidromePermutation.java
@@ -12,3 +12,23 @@ class Solution {
         return contain.size() == 1 || contain.size() == 0;
     }
 }
+//method2
+//我们只关注对于odd个数的元素的个数是不是一
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        int[]marker = new int[26];
+        for(char c : s.toCharArray()){
+            marker[c - 'a']++;
+        }
+        boolean oddApper = false;
+        for(int ele : marker){
+            if(ele % 2 == 1){
+                if(oddApper){
+                    return false;
+                }
+                oddApper = true;
+            }
+        }
+        return true;
+    }
+}
