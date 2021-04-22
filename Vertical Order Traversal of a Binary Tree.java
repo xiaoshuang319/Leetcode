154a8b5dbb35fde0bfbacad95e501a6c87b1a246
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-04-22 08:35:35 -0700

    Update Vertical Order Traversal of a Binary Tree.java

--- Vertical Order Traversal of a Binary Tree.java
+++ Vertical Order Traversal of a Binary Tree.java
@@ -50,3 +50,68 @@ class Solution {
         helper(node.right, col + 1, row + 1,colToNode);
     }
 }
+
+//similar: Binary Tree Vertical Order Traversal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        if(root == null){
+            return new ArrayList<>();
+        }
+        List<List<Integer>>result = new ArrayList<>();
+        int height = getHeight(root);
+        int totalBuckets = 2 * height - 1;
+        for(int i = 0; i < totalBuckets; i++){
+            List<Integer>temp = new ArrayList<>();
+            result.add(temp);
+        }
+        int midIndex = height - 1;
+      
+        Queue<Node>queue =  new LinkedList<>();
+        queue.add(new Node(root, midIndex));
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                Node curr = queue.poll();
+                int currIndex = curr.index;
+                 result.get(currIndex).add(curr.node.val);
+                if(curr.node.left != null ){
+                    queue.add(new Node(curr.node.left,currIndex - 1));
+                }
+                if(curr.node.right != null){
+                   queue.add(new Node(curr.node.right, currIndex + 1));
+                }
+            }
+        }
+        result.removeIf(ele -> ele.size() == 0);
+        return result;
+    }
+    private int getHeight(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        return Math.max(getHeight(node.left),getHeight(node.right)) + 1;
+    }
+    private class Node{
+        TreeNode node;
+        int index;
+        public Node(TreeNode node, int index){
+            this.node = node;
+            this.index = index;
+        }
+    }
+}
