7156d0714491a06206f7bef63630e98a2785a7a7
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-04-26 16:22:09 -0700

    Update Find First and Last Position of Element in Sorted Array.java

--- Find First and Last Position of Element in Sorted Array.java
+++ Find First and Last Position of Element in Sorted Array.java
@@ -98,4 +98,26 @@ class Solution {
       }
       return pos;
     }
+} //find the low bound of subarray with length k by comparing x - a[mid] with a[mid+k]-x
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+       
+        int start = 0, end = arr.length - k; //the possible range of lowr bound
+        while(start < end){
+             int mid = start + (end - start) /2;
+            if(x - arr[mid] > arr[mid + k] - x){
+               start = mid + 1;
+            }else{
+               end = mid;
+            }
+        }
+        List<Integer>result = new ArrayList<>();
+        for(int i = start; i < start + k; i++){
+            result.add(arr[i]);
+        }
+        return result;
+    }
+   
 }
+
+
