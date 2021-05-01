0b7a8b151553088b1463c885ae8fba5dc612ca73
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-01 12:05:02 -0700

    Update longest substring with k distinct character.java

--- longest substring with k distinct character.java
+++ longest substring with k distinct character.java
@@ -56,3 +56,20 @@ class Solution {
         return result;
     }
 }
+//一摸一样
+// Max Consecutive Ones III
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0, len = nums.length, zeroCount = 0, result = 0;
+        for(int right = 0; right < len; right++){
+            zeroCount += nums[right] == 0 ? 1 : 0;
+            while(zeroCount > k){
+                zeroCount -= nums[left] == 0 ? 1 : 0;
+                left++;
+            }
+            result = Math.max(result, right - left + 1);
+        }
+        return result;
+    }
+}
