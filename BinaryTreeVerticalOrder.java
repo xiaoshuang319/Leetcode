92651066c3677a6b5a95a36ce11281ecb8fef884
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-03 22:51:01 -0700

    Update BinaryTreeVerticalOrder.java

--- BinaryTreeVerticalOrder.java
+++ BinaryTreeVerticalOrder.java
@@ -62,3 +62,33 @@ class Solution {
     }
     
 }
+//题目：level order
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null){
+            return new ArrayList<>();
+        }
+        List<List<Integer>>result = new ArrayList<>();
+        Queue<TreeNode>queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer>temp = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode curr = queue.poll();
+                temp.add(curr.val);
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+                if(curr.right != null){
+                    queue.add(curr.right);
+                }
+
+ 
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+}
