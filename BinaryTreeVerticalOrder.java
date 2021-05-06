8d7ae62323979d61908d8405def3818ec295746b
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-06 01:18:47 -0700

    Update BinaryTreeVerticalOrder.java

--- BinaryTreeVerticalOrder.java
+++ BinaryTreeVerticalOrder.java
@@ -92,3 +92,54 @@ class Solution {
         return result;
     }
 }
+////题目：lzigzagLevelOrder
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if(root== null)return new ArrayList<>();
+        List<List<Integer>>result = new ArrayList<>();
+        Queue<TreeNode>queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode curr = queue.poll();
+                
+                if(result.size() == level){
+                    List<Integer>temp = new ArrayList<>();
+                    temp.add(curr.val);
+                    result.add(temp);
+                }else if(level % 2 != 0){
+                    result.get(level).add(0,curr.val);
+                }else{
+                 result.get(level).add(curr.val);
+                }
+                
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+                if(curr.right != null){
+                    queue.add(curr.right);
+                }
+            }
+            level++;
+        }
+        return result;
+    }
+}
