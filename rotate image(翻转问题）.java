7409ae151e6562f0e8495933629ee3f68597b1a3
xiaoshuang319 <xiaoshuang319@gmail.com>
2021-05-28 11:08:18 -0700

    Update rotate image(翻转问题）.java

--- rotate image(翻转问题）.java
+++ rotate image(翻转问题）.java
@@ -18,3 +18,23 @@ class Solution {
         }
     }
 }
+
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k = k % len;
+        reverse(nums, 0, len - 1); //rerver entire array
+        reverse(nums,0,k - 1); //reverse subarray from 0 to k - 1,
+        reverse(nums,k, len - 1);//reverse subarray from k to len - 1,
+    }
+    private void reverse(int[]nums, int start, int end){
+        while(start <= end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
